07d4d766bdbf38775ccd9afb88f089e763201db2
quangtinator <phungvunhatquang@gmail.com>
2021-12-11 23:53:45 +0100

    test

--- backend/app.js
+++ backend/app.js
@@ -407,7 +407,7 @@ const clearReport = () => {
     fs.writeFile('report.csv', '', function(){console.log('Report cleared!!')})
 }
 
-cron.schedule('0 9 * * 6', () => {
+cron.schedule('0 9 * * 1', () => {
     console.log('---------------------');
     console.log('Running Cron Job');
     writeReport();
